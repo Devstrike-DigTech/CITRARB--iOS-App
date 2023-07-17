c1c9e4b6fe894cee1b55786e45fd20f2064bf78c
Richard Uzor <43550320+EQua-Dev@users.noreply.github.com>
2023-07-17 08:03:41 +0100

    Initial Commit

---
+++ CITRARB/CITRARBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CITRARBApp.swift
+//  CITRARB
+//
+//  Created by Richard Uzor on 17/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct CITRARBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
